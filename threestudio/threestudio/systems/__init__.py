0fce7b81f7f896902aaa01dd0aa4ca534bf90636
Junha Jang <andy.jang@kaist.ac.kr>
2025-05-29 03:48:43 +0900

    docs: create threestudio-animate124 repository & outputs

---
+++ threestudio/threestudio/systems/__init__.py
@@ -0,0 +1,17 @@
+from . import (
+    control4d_multiview,
+    dreamfusion,
+    eff_dreamfusion,
+    fantasia3d,
+    imagedreamfusion,
+    instructnerf2nerf,
+    latentnerf,
+    magic3d,
+    magic123,
+    prolificdreamer,
+    sdi,
+    sjc,
+    textmesh,
+    zero123,
+    zero123_simple,
+)
