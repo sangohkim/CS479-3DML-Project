c17b276f586936ddba2f55d36c690a44bb0452b4
Sang-oh Kim <tkddh1109@gmail.com>
2025-05-29 03:18:33 +0900

    Fix camera coordinate

--- process_video.sh
+++ process_video.sh
@@ -2,4 +2,5 @@
 
 python process_video.py \
     /Users/sangohkim/ksodev/cs479/CS479-3DML-Project/animate-124/magic-scarecrow/5-stage3/it5000-test_static \
-    /Users/sangohkim/ksodev/cs479/CS479-3DML-Project/animate-124/howl-markl/5-stage3/it5000-test_static
+    /Users/sangohkim/ksodev/cs479/CS479-3DML-Project/animate-124/howl-markl/5-stage3/it5000-test_static \
+    /Users/sangohkim/ksodev/cs479/CS479-3DML-Project/animate-124/howl-castle/5-stage3/it5000-test
