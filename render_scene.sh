346765b6925c6868e3385c1994e95c70695dc2ed
sangohkim <tkddh1109@kaist.ac.kr>
2025-05-31 14:25:00 +0000

    Add final

---
+++ render_scene.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ns-render camera-path --load-config outputs/final_scene_7/splatfacto-w-light/2025-05-31_124546/config.yml --camera-path-filename /root/3D-Rendering-Contest/CS479-3DML-Project/data/final_scene_7/camera_paths/2025-05-31-13-38-34.json --output-path renders/final_scene_7/2025-05-31-13-38-34.mp4
---
+++ train_splatfacto.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ns-train splatfacto-w-light --data data/final_scene_7
