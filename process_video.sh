026375ae37b75dc1f78ae824d75b849e2f173028
sangohkim <tkddh1109@kaist.ac.kr>
2025-05-31 14:41:54 +0000

    final

--- process_video.sh
+++ process_video.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python process_video.py \
-    ./animate-124/magic-scarecrow/5-stage3/it5000-test_static \
-    ./animate-124/howl-markl/5-stage3/it5000-test_static \
-    ./animate-124/howl-castle/5-stage3/it5000-test \
-    ./animate-124/spring-thief/5-stage3/it5000-test \
+    threestudio/outputs/animate124-stage3/a_scarecrow_is_wobbling_down_a_path@20250524-000230/it5000-test_static \
+    /root/3D-Rendering-Contest/CS479-3DML-Project/threestudio/outputs/animate124-stage3/a_boy_jumping_joyfully_with_spiky_hair,_green_vest_fluttering,_and_red_pants@20250524-000230/it5000-test_static \
+    threestudio/outputs/animate124-stage3/a_castle_walking_across_a_meadow,_smoke_rising_from_its_chimneys@20250524-052103/it5000-test \
+    threestudio/outputs/animate124-stage3/a_cute_shiba_inu_dog_is_sitting_and_panting_while_looking_at_the_camera,_under_cherry_blossoms,_cartoon_style@20250519-072958/it5000-test \
